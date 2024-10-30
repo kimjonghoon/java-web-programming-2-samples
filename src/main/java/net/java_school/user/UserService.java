8d5125179c4fdc5ce06d5ea72982e6a02ec554af
Jonghoon Kim <kim0070@gmail.com>
2024-10-30 12:17:17 +0900

    1030 office updated

--- src/main/java/net/java_school/user/UserService.java
+++ src/main/java/net/java_school/user/UserService.java
@@ -1,12 +1,17 @@
 package net.java_school.user;
 
+import org.springframework.security.access.method.P;
 import org.springframework.security.access.prepost.PreAuthorize;
 
 public interface UserService {
+
 	public int addUser(User user);
+
 	public int addAuthority(String username, String authority);
+
 	@PreAuthorize("hasAnyRole('ADMIN','USER')")
 	public int changePassword(String currentPassword, String newPassword, String username);
-	@PreAuthorize("hasAnyRole('ADMIN','USER')")
-	public String getPassword(String username);
+
+	@PreAuthorize("hasRole('ADMIN') or #username == principal.username")
+	public String getPassword(@P("username") String username);
 }
