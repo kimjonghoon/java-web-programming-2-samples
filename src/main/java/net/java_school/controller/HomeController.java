2a1f8f0e1611b4c514c1f47c38b17c149be5e6f1
Jonghoon Kim <kim0070@gmail.com>
2024-11-06 16:33:34 +0900

    HomeController.java updated

--- src/main/java/net/java_school/controller/HomeController.java
+++ src/main/java/net/java_school/controller/HomeController.java
@@ -2,6 +2,7 @@ package net.java_school.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class HomeController {
@@ -10,4 +11,14 @@ public class HomeController {
 	public String index() {
 		return "index";
 	}
+	
+	@GetMapping("thymeleaf")
+	public String thymeleafHome() {
+		return "thymeleaf/index";
+	}
+	
+	@GetMapping("thymeleaf/{section}")
+	public String getSection(@PathVariable("section") String section) {
+		return "thymeleaf/" + section;
+	}	
 }
