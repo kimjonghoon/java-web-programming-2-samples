0124d01e126f830ab290238c443f42bc709a1814
Jonghoon Kim <kim0070@gmail.com>
2024-11-07 15:07:36 +0900

    1107 office updated

--- src/main/java/net/java_school/controller/HomeController.java
+++ src/main/java/net/java_school/controller/HomeController.java
@@ -3,6 +3,7 @@ package net.java_school.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.ui.Model;
 
 @Controller
 public class HomeController {
@@ -12,6 +13,12 @@ public class HomeController {
 		return "index";
 	}
 	
+	@GetMapping("{lang:en|ko}")
+	public String indexByLang(@PathVariable("lang") String lang, Model model) {
+		model.addAttribute("lang", lang);
+		return lang + "/index";
+	}
+
 	@GetMapping("{chapter:thymeleaf|spring-security}")
 	public String chapterIndex(@PathVariable("chapter") String chapter) {
 		return chapter + "/index";
@@ -21,4 +28,17 @@ public class HomeController {
 	public String getSection(@PathVariable("chapter") String chapter, @PathVariable("section") String section) {
 		return chapter + "/" + section;
 	}
+
+	@GetMapping("{lang:en|ko}/{chapter:thymeleaf|spring-security}")
+	public String chapterIndexByLang(@PathVariable("lang") String lang, @PathVariable("chapter") String chapter, Model model) {
+		model.addAttribute("lang", lang);
+		return lang + "/" + chapter + "/index";
+	}
+
+	@GetMapping("{lang:en|ko}/{chapter:thymeleaf|spring-security}/{section}")
+	public String getSectionByLang(@PathVariable("lang") String lang, @PathVariable("chapter") String chapter, @PathVariable("section") String section, Model model) {
+		model.addAttribute("lang", lang);
+		return lang + "/" + chapter + "/" + section;
+	}
+
 }
