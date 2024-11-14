789c28bcfac420f038e1c0073b342ef7b29d331a
Jonghoon Kim <kim0070@gmail.com>
2024-11-14 10:39:43 +0900

    1114 home

--- src/main/java/net/java_school/controller/HomeController.java
+++ src/main/java/net/java_school/controller/HomeController.java
@@ -12,28 +12,26 @@ public class HomeController {
 	public String index() {
 		return "index";
 	}
-
-	@GetMapping("thymeleaf/")
-	public String test() {
-		return "thymeleaf/index";
+	@GetMapping("{lang:en|ko}")
+	public String indexByLang(@PathVariable("lang") String lang, Model model) {
+		model.addAttribute("lang", lang);
+		return lang + "/index";
 	}
-	@GetMapping("thymeleaf")
-	public String indexThymeleaf() {
-		return "thymeleaf/index";
-	}
-	
-	@GetMapping("thymeleaf/{section}")
-	public String getThymeleafSection(@PathVariable("section") String section) {
-		return "thymeleaf/" + section;
-	}
-
-	@GetMapping("spring-security")
-	public String indexSpringSecurity() {
-		return "spring-security/index";
+	@GetMapping(value={"{chapter:thymeleaf|spring-security}", "{chapter:thymeleaf|spring-security}/{section}"})
+	public String getSection(@PathVariable("chapter") String chapter, @PathVariable(value="section", required=false) String section) {
+		if (section != null) {
+			return chapter + "/" + section;
+		} else {
+			return chapter + "/index";
+		}
 	}
 	
-	@GetMapping("spring-security/{section}")
-	public String getSpringSecuritySection(@PathVariable("section") String section) {
-		return "spring-security/" + section;
+	@GetMapping(value={"{lang:en|ko}/{chapter:thymeleaf|spring-security}", "{lang:en|ko}/{chapter:thymeleaf|spring-security}/{section}"})
+	public String getSectionByLang(@PathVariable("lang") String lang, @PathVariable("chapter") String chapter, @PathVariable(value="section", required=false) String section, Model model) {
+		model.addAttribute("lang", lang);
+		if (section != null)
+			return lang + "/" + chapter + "/" + section;
+		else
+			return lang + "/" + chapter + "/index";
 	}
 }
