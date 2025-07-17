298f27c04715ddfdd606b34b673ec43e76e7709b
Jonghoon Kim <kim0070@gmail.com>
2025-07-18 04:30:47 +0900

    User.java updated and dependencies updated

--- src/main/java/net/java_school/user/User.java
+++ src/main/java/net/java_school/user/User.java
@@ -13,7 +13,7 @@ public class User {
   @Size(min=4, message="{password.validation.error}")
   private String password;
 	
-  private Integer enabled;
+  //private Integer enabled;
 
   private List<String> authorities;
 
@@ -21,7 +21,7 @@ public class User {
   public User(String username, String password, Integer enabled, List<String> authorities) {
     this.username = username;
     this.password = password;
-    this.enabled = enabled;
+    //this.enabled = enabled;
     this.authorities = authorities;
   }
   public String getUsername() {
@@ -36,12 +36,14 @@ public class User {
   public void setPassword(String password) {
     this.password = password;
   }
+  /*
   public Integer getEnabled() {
     return enabled;
   }
   public void setEnabled(Integer enabled) {
     this.enabled = enabled;
   }
+  */
   public List<String> getAuthorities() {
     return authorities;
   }
