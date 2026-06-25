e623f7ed74e061bccfb08049dd16e0f954580fe0
Jonghoon Kim <kim0070@gmail.com>
2026-06-25 19:17:09 +0900

    PreAuthorize added in BlogService

--- src/main/java/net/java_school/blog/BlogService.java
+++ src/main/java/net/java_school/blog/BlogService.java
@@ -2,17 +2,23 @@ package net.java_school.blog;
 
 import java.util.List;
 
+import org.springframework.security.access.prepost.PreAuthorize;
+
 public interface BlogService {
 
 	public List<Blog> getAll();
 	
 	public Blog getOne(String slug);
 	
+	@PreAuthorize("hasRole('ADMIN')")
 	public int changeSlug(Blog blog);
 	
+	@PreAuthorize("hasRole('ADMIN')")
 	public int changeDescription(Blog blog);
 	
+	@PreAuthorize("hasRole('ADMIN')")
 	public int addBlog(Blog blog);
 	
+	@PreAuthorize("hasRole('ADMIN')")
 	public int removeBlog(int postNo);
 }
--- src/main/java/net/java_school/controller/BlogController.java
+++ src/main/java/net/java_school/controller/BlogController.java
@@ -18,7 +18,6 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.ui.Model;
 
@@ -92,7 +91,7 @@ public class BlogController {
 	
 	@PatchMapping("blog/{postNo}")
 	@ResponseStatus(code=HttpStatus.NO_CONTENT)
-	public void patchSlug(@PathVariable(name="postNo") int postNo, @RequestBody Blog blog) {
+	public void patchSlugAndDescription(@PathVariable(name="postNo") int postNo, @RequestBody Blog blog) {
 		blog.setPostNo(postNo);
 		if (blog.getSlug() != null) {
 			blogService.changeSlug(blog);
